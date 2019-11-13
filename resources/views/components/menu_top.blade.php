f4701900278983bf2205cc7eb30d42d697adc0a5
sawatzki <artsawatzki@gmail.com>
2019-11-13 09:46:35 +0100

    services

--- resources/views/components/menu_top.blade.php
+++
@@ -1,9 +0,0 @@
-<li class="nav-item active">
-    <a class="nav-link" href="#">HOME<span class="sr-only">(current)</span></a>
-</li>
-<li class="nav-item">
-    <a class="nav-link" href="#">SHOP</a>
-</li>
-<li class="nav-item">
-    <a class="nav-link" href="#">CONTACTS</a>
-</li>
