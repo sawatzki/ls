9c0c4932960eb0bfca8bf99d1b338393981258e9
sawatzki <artsawatzki@gmail.com>
2019-11-13 10:16:09 +0100

    movie - model und migration erstellt

---
+++ database/migrations/2019_11_13_091111_create_movies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->integer('category_id');
+            $table->integer('author_id');
+            $table->string('title')->nullable();
+            $table->decimal('price');
+            $table->string('language')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
